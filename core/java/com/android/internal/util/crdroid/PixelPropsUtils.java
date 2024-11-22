3e136760cf54f553b0d3e5c28c01911f74da28f4
darkphnx <dark.phnx12@gmail.com>
2024-11-22 17:53:27 +0000

    PPUtils:add option to keystore safetynet attestation blocking [1/2] *for Users who want to use custom pif and tricky box

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -42,6 +42,7 @@ public class PixelPropsUtils {
     private static final boolean DEBUG = SystemProperties.getBoolean(PROP_HOOKS + "DEBUG", false);
 
     private static final String SPOOF_PIXEL_GMS = "persist.sys.pixelprops.gms";
+    private static final String SPOOF_PIXEL_PI = "persist.sys.pixelprops.pi";
     private static final String SPOOF_PIXEL_GPHOTOS = "persist.sys.pixelprops.gphotos";
     private static final String SPOOF_PIXEL_NETFLIX = "persist.sys.pixelprops.netflix";
     private static final String ENABLE_PROP_OPTIONS = "persist.sys.pixelprops.all";
@@ -121,7 +122,6 @@ public class PixelPropsUtils {
             "com.google.android.apps.nexuslauncher",
             "com.google.android.inputmethod.latin",
             "com.google.android.tts",
-            "com.google.android.wallpaper.effects"
             "com.netflix.mediaclient"
         };
 
@@ -139,11 +139,9 @@ public class PixelPropsUtils {
             if (SystemProperties.getBoolean(SPOOF_PIXEL_GPHOTOS, true)) {
                 propsToChange.putAll(propsToChangePixelXL);
             } else {
-                if (!isPixelDevice) {
                     propsToChange.putAll(propsToChangePixel8Pro);
                 }
             }
-        }
 
         if (packageName.equals("com.google.android.gms")) {
             if (SystemProperties.getBoolean(SPOOF_PIXEL_GMS, true)) {
@@ -151,7 +149,7 @@ public class PixelPropsUtils {
                     return;
                 }
             }
-        }
+	}
 
         if (!propsToChange.isEmpty()) {
             if (DEBUG) Log.d(TAG, "Defining props for: " + packageName);
@@ -309,6 +307,10 @@ public class PixelPropsUtils {
     }
 
     public static void onEngineGetCertificateChain() {
+
+        if (!SystemProperties.getBoolean(SPOOF_PIXEL_PI, true))
+            return;
+
         // Check stack for SafetyNet or Play Integrity
         if (isCallerSafetyNet()) {
             Log.i(TAG, "Blocked key attestation");
