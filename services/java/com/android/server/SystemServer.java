41d9b314d6ba6b732cb0b1296ec8dab9559ffe19
darkphnx <dark.phnx12@gmail.com>
2024-12-19 16:18:39 +0000

    Revert "PixelPropsUtils: Dynamically spoof props for GMS"
    This reverts commit a10431dfe70f23d277ec8fee67041bc149b800e7.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -154,7 +154,6 @@ import com.android.server.contentsuggestions.ContentSuggestionsManagerService;
 import com.android.server.contextualsearch.ContextualSearchManagerService;
 import com.android.server.coverage.CoverageService;
 import com.android.server.cpu.CpuMonitorService;
-import com.android.server.crdroid.AttestationService;
 import com.android.server.crdroid.CustomDeviceConfigService;
 import com.android.server.credentials.CredentialManagerService;
 import com.android.server.criticalevents.CriticalEventLog;
@@ -2681,11 +2680,6 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartBackgroundInstallControlService");
             mSystemServiceManager.startService(BackgroundInstallControlService.class);
-            t.traceEnd();
-
-            // AttestationService
-            t.traceBegin("AttestationService");
-            mSystemServiceManager.startService(AttestationService.class);
             t.traceEnd();
 
             // CustomDeviceConfigService
